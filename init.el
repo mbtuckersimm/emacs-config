ead2f4f1422a8cf55be60fee405412937a189b92
matthew <tucker.simmons@gmail.com>
2017-03-22 16:53:31 -0700

    a few more changes

--- init.el
+++ init.el
@@ -24,6 +24,7 @@
                           ("gnu" . "http://elpa.gnu.org/packages/")
                           ("marmalade" . "http://marmalade-repo.org/packages/")
 			  ("melpa" . "http://melpa.milkbox.net/packages/")))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
