2f06a56c9875644e5e7c027306c063b91510abfb
Matthew Tucker-Simmons <tucker.simmons@gmail.com>
2019-01-08 10:09:52 -0800

    use org-tempo package to reinstate support for easy templates

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;;; init.el --- Summary
 ;;; Author: Matthew Tucker-Simmons
-;;; Time-stamp: <2018-12-10 12:52:32 matthew>
+;;; Time-stamp: <2019-01-07 12:52:40 matthew>
 
 ;;; Commentary:
 ;;; This is only here to stop flycheck from giving me a warning.
@@ -309,6 +309,9 @@
 (use-package org-bullets
   :hook (org-mode . org-bullets-mode)
   :ensure t)
+
+;; trigger easy templates via (eg) `< s TAB'
+(use-package org-tempo)
 
 ;; move these into :bindings section of use-package form
 (global-set-key (kbd "C-c l") 'org-store-link)
