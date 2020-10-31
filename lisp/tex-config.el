42fd09fda38addc467b0ce25823ad0fc615e885f
Matthew Tucker-Simmons <tucker.simmons@gmail.com>
2020-10-31 13:43:50 -0700

    move more stuff out to tex-config.el

--- lisp/tex-config.el
+++ lisp/tex-config.el
@@ -1,3 +1,62 @@
+;; bunch of stuff removed from custom.el
+
+ '(TeX-save-query nil)
+ '(TeX-source-correlate-method 'synctex)
+ '(TeX-source-correlate-mode t)
+ '(TeX-source-correlate-start-server t)
+ '(TeX-view-program-list '(("Ok" "okular --unique %o#src:%n`pwd`/%b")))
+ '(TeX-view-program-selection
+   '(((output-dvi has-no-display-manager)
+      "dvi2tty")
+     ((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "Ok")
+     (output-html "xdg-open")))
+ '(bibtex-entry-format '(opts-or-alts required-fields))
+ '(bibtex-generate-url-list
+   '((("url" . ".*:.*"))
+     (("doi" . "10\\.[0-9]+/.+")
+      "http://dx.doi.org/%s"
+      ("doi" ".*" 0))
+     (("mrkey" . "\\(mr\\)?[0-9]\\{1,8\\}")
+      "http://mathscinet.ams.org/mathscinet-getitem?mr=%s"
+      ("mrkey" "\\(mr\\)?\\([0-9]\\{1,8\\}\\)" 2))
+     (("mrnumber" . "\\(mr\\)?[0-9]\\{1,8\\}")
+      "http://mathscinet.ams.org/mathscinet-getitem?mr=%s"
+      ("mrnumber" "\\(mr\\)?\\([0-9]\\{1,8\\}\\)" 2))
+     (("arxiv" . "\\([0-9]\\{4\\}\\.[0-9]\\{4,5\\}\\|[-a-z]+/[0-9]\\{7\\}\\|[-a-z]+\\.[a-z]\\{2\\}/[0-9]\\{7\\}\\)\\(v[0-9]+\\)?")
+      "http://arxiv.org/abs/%s"
+      ("arxiv" ".*" 0))
+     (("zblnumber" . "\\([0-9]\\{4\\}\\.[0-9]\\{5\\}\\|[0-9]\\{8\\}\\)")
+      "http://zbmath.org/?q=an:%s"
+      ("zblnumber" ".*" 0))))
+
+
+(use-package bibtex
+  :custom
+  (bibtex-generate-url-list
+   (quote
+    ((("url" . ".*:.*"))
+     (("doi" . "10\\.[0-9]+/.+")
+      "http://dx.doi.org/%s"
+      ("doi" ".*" 0))
+     (("mrkey" . "\\(mr\\)?[0-9]\\{1,8\\}")
+      "http://mathscinet.ams.org/mathscinet-getitem?mr=%s"
+      ("mrkey" "\\(mr\\)?\\([0-9]\\{1,8\\}\\)" 2))
+     (("mrnumber" . "\\(mr\\)?[0-9]\\{1,8\\}")
+      "http://mathscinet.ams.org/mathscinet-getitem?mr=%s"
+      ("mrnumber" "\\(mr\\)?\\([0-9]\\{1,8\\}\\)" 2))
+     (("arxiv" . "\\([0-9]\\{4\\}\\.[0-9]\\{4,5\\}\\|[-a-z]+/[0-9]\\{7\\}\\|[-a-z]+\\.[a-z]\\{2\\}/[0-9]\\{7\\}\\)\\(v[0-9]+\\)?")
+      "http://arxiv.org/abs/%s"
+      ("arxiv" ".*" 0))
+     (("zblnumber" . "\\([0-9]\\{4\\}\\.[0-9]\\{5\\}\\|[0-9]\\{8\\}\\)")
+      "http://zbmath.org/?q=an:%s"
+      ("zblnumber" ".*" 0))))))
+
+
+
+
 ;;; AUCTeX AND MATH SETTINGS ;;;
 
 ;; make $ and $$ pair properly in LaTeX mode
