a353d5d2efe9bbb4b3f35b2569872c8f97b6eb6b
Matthew Tucker-Simmons <tucker.simmons@gmail.com>
2020-12-03 17:24:29 -0800

    ignore jQuery, js and css bundles, and minified js files when grepping

--- custom.el
+++ custom.el
@@ -45,6 +45,10 @@
  '(explicit-shell-file-name "/bin/bash")
  '(fci-rule-color "#383838")
  '(font-lock-maximum-decoration t)
+ '(grep-find-ignored-directories
+   '("SCCS" "RCS" "CVS" "MCVS" ".src" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "js/bundles" "css/bundles" "js/jQuery"))
+ '(grep-find-ignored-files
+   '(".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo" "*.min.js"))
  '(helm-M-x-fuzzy-match t t)
  '(helm-M-x-requires-pattern 0 t)
  '(helm-boring-buffer-regexp-list
